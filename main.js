a1886078fb8a6ac22b951a871e778ffe466f07fb
vigram99 <62041545+vigram99@users.noreply.github.com>
2020-05-14 20:38:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const siema = new Siema();
-var btnNext = document.querySelectorAll("#next");
-var btnPrev = document.querySelectorAll("#prev");
 var btnOrder = document.querySelectorAll("#claim_btn");
 var counter = 0;
 var counterBtnOrder = 0;
@@ -13,40 +10,3 @@ while (counterBtnOrder < btnOrder.length) {
     });
     counterBtnOrder++;
 }
-
-
-while (counter < btnNext.length) {
-    btnNext[counter].addEventListener('click', () => siema.next());
-    btnPrev[counter].addEventListener('click', () => siema.prev());
-    counter++;
-}
-
-
-
-function gTag() {
-
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-
-    gtag('config', 'AW-688116829');
-
-}
-
-
-function gtag_report_conversion(url) {
-    var callback = function() {
-        if (typeof(url) != 'undefined') {
-            window.location = url;
-        }
-    };
-    gtag('event', 'conversion', {
-        'send_to': 'AW-688116829/wWOiCIOO_MwBEN2oj8gC',
-        'transaction_id': '',
-        'event_callback': callback
-    });
-    return false;
-}
-
-gTag();
