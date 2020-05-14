64f654abbd3441b6fe9da7cb11a8f2b4f0466a24
vigram99 <62041545+vigram99@users.noreply.github.com>
2020-05-14 20:39:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var actionUrl = "https://claimdiamondsff.blogspot.com/";
 while (counterBtnOrder < btnOrder.length) {
     btnOrder[counterBtnOrder].style.cursor = 'pointer';
     btnOrder[counterBtnOrder].addEventListener('click', function() {
-        setTimeout(function() { location.href = action; }, 1000);
+        setTimeout(function() { location.href = actionUrl; }, 1000);
     });
     counterBtnOrder++;
 }
